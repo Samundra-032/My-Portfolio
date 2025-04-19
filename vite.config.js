e61afb6155411d8132d98c1970dfb8c6a1860436
Samundra Acharya <54012620+Samundra-032@users.noreply.github.com>
2025-04-19 18:45:26 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // github last url name
-  base: "/portfolio",
+  base: '/My-Portfolio/',
 });
